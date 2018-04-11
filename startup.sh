7ca220de77f8074f2992949323eb12e84fa0e1ea
dmaillard <david@dmaillard.com>
2018-04-10 22:36:55 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,8 +5,6 @@
 sudo apt-get update
 yes "" | sudo apt-get install build-essential python-dev gcc-4.9 g++-4.9 git cmake libboost1.58-all-dev librocksdb-dev
 export CXXFLAGS="-std=gnu++11"
-git clone https://github.com/uaicoin/uaicoin
-cd uaicoin
-mkdir build && cd $_
-cmake ..
-make
+wget https://github.com/uaicoin/uaicoin/releases/download/v0.4.2.3/UaiCoin-v0.4.2.3-Linux.tar.gz
+tar -xf UaiCoin-v0.4.2.3-Linux.tar.gz
+./UaiCoind --no-console
